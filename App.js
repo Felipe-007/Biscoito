b6a44cba6d4f89005920c0a1043516b1949b67e3
Felipe Lopes <felipe.presidencia@gmail.com>
2022-12-20 11:15:58 -0300

    C1 - Quebrar biscoito

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      textoFrase: '',
+      img: require('./src/biscoito.png'),
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+
+  quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)  //conta quantas frases existem
+
+    this.setState({
+      textoFrase: this.frases[numeroAleatorio],  //apresenta a frase aleatoria
+      img: require('./src/biscoitoAberto.png'),  //altera a imagem
+    })
+  }
+
+  render() {
+    return (
       <View style={styles.container}>
-        <Text>Biscoito</Text>
+        <Image
+          style={styles.imagem}
+          source={this.state.img}
+        />
+
+        <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={ this.quebraBiscoito }>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Quebrar biscoito</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
--- styles.js
+++ styles.js
@@ -3,5 +3,36 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagem: {
+    width: 250,
+    height: 250,
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  btnTexto: {
+    fontSize: 18,
+    color: '#dd7b22',
+    fontWeight: 'bold'
   }
 })
